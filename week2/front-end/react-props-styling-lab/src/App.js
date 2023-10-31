9b99fbc5ca0d60b243f0590b3a786431a07ad957
Kim Löfgren <lofgrenkim@outlook.com>
2023-10-31 11:40:02 +0200

    props tests

--- week2/front-end/react-props-styling-lab/src/App.js
+++ week2/front-end/react-props-styling-lab/src/App.js
@@ -4,7 +4,7 @@ import "./App.css"; // import the CSS file
 function App() {
   return (
     <div className="App">
-      <Greeting name="Sara" message="Welcome to the lab!" />
+      <Greeting item1="banana"name="Sara" message="Welcome to the lab!" />
       <Greeting name="Cahlan" message="good morning!" />
       <Greeting name="Edgar" message="what is this?" />
     </div>
--- week2/front-end/react-props-styling-lab/src/Greeting.js
+++ week2/front-end/react-props-styling-lab/src/Greeting.js
@@ -5,6 +5,7 @@ function Greeting(props) {
     <div>
       <h1> Welcome, {props.name}!</h1>
       <p>{props.message}</p>
+      <p>Item 1 : {props.item1}</p>
     </div>
   );
 }
