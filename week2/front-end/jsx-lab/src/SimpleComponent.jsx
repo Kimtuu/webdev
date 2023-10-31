94cca4235858c51dd07414f53c656943063cf45b
Kim Löfgren <lofgrenkim@outlook.com>
2023-10-31 10:15:47 +0200

    jsx lab

---
+++ week2/front-end/jsx-lab/src/SimpleComponent.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+// define a simple functional component
+
+function SimpleComponent() {
+  return (
+    <div>
+      <h1>Hello, JSX lab!</h1>
+      <p>This is a simple React component using JSX.</p>
+    </div>
+  );
+}
+
+export default SimpleComponent;
