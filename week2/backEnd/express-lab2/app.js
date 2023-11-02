8abfd21321523b4d6c8011192c4c4909b702c5c4
Kim Löfgren <lofgrenkim@outlook.com>
2023-11-02 10:32:28 +0200

    lab2

---
+++ week2/backEnd/express-lab2/app.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const app = express();
+const port = 4000;
+
+// Endpoint 1: Text Response
+app.get("/textmessage", (req, res) => {
+  res.send("This is a simple textmessage response.");
+});
+
+app.get("/jsonmessage", (req, res) => {
+  const jSonData = {
+    message: "this is an again updated JSON response",
+    timestamp: new Date(),
+  };
+  res.json(jSonData);
+});
+
+app.get("/htmlmessage", (req, res) => {
+  res.send("<h1>This is a simple html response.</h1>");
+});
+
+app.get("/info", (req, res) => {
+  let time = new Date();
+  res.send(`
+    <h1>Info Page</h1>
+    ${time.toString()}
+    `);
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
