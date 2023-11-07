afadfbb5bae8779aacd810d1d25f4ec9af2262bc
Kim Löfgren <lofgrenkim@outlook.com>
2023-11-07 09:37:47 +0200

    w3 activity pt1

---
+++ week3/front/javascript-concepts-lab/destructuring.js
@@ -0,0 +1,6 @@
+const Person = { name: "John", age: 21, location: "London" };
+
+const { name, age } = Person;
+
+console.log("Name:", name);
+console.log("Age:", age);
---
+++ week3/front/javascript-concepts-lab/spread.js
@@ -0,0 +1,9 @@
+const arr1 = [1, 2, 3];
+const arr2 = [4, 5, 6];
+
+// combine the arrays into a single array
+const combinedArray = [...arr1, ...arr2];
+
+console.log("Array 1:", arr1);
+console.log("Array 2:", arr2);
+console.log("Combined Array:", combinedArray);
