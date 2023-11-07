b57611628a4a8ac11cda09999b30f8d85335908a
Kim Löfgren <lofgrenkim@outlook.com>
2023-11-07 12:35:21 +0200

    Delete Recipe and data components, and remove RecipeList import from index.js

--- week3/front/react-lists-lab/src/index.js
+++ week3/front/react-lists-lab/src/index.js
@@ -2,12 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './components/Book.css'
-import RecipeList from './Recipe';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
-    <RecipeList />
   </React.StrictMode>
 );
