f9b999dcbb1fe913ed8c332afac8b88b29c950f1
Kim Löfgren <lofgrenkim@outlook.com>
2023-11-22 20:33:17 +0200

    MERN tutorial 12

--- week5/training/mern-stack/frontend/src/context/WorkoutContext.js
+++ week5/training/mern-stack/frontend/src/context/WorkoutContext.js
@@ -2,20 +2,24 @@ import { createContext, useReducer } from "react";
 
 export const WorkoutContext = createContext();
 
-export const workoutsReducer =  (state, action) => {
-    switch (action.type) {
-        case "SET_WORKOUTS":
-            return {
-                workouts: action.payload
-            }
-        case "CREATE_WORKOUT":
-            return {
-                workouts: [action.payload, ...state.workouts]
-            }
-        default:
-            return state
-    }
-}
+export const workoutsReducer = (state, action) => {
+  switch (action.type) {
+    case "SET_WORKOUTS":
+      return {
+        workouts: action.payload,
+      };
+    case "CREATE_WORKOUT":
+      return {
+        workouts: [action.payload, ...state.workouts],
+      };
+    case "DELETE_WORKOUT":
+      return {
+        workouts: state.workouts.filter((w) => w._id !== action.payload._id),
+      };
+    default:
+      return state;
+  }
+};
 
 export const WorkoutContextProvider = ({ children }) => {
   const [state, dispatch] = useReducer(workoutsReducer, {
@@ -23,7 +27,7 @@ export const WorkoutContextProvider = ({ children }) => {
   });
 
   return (
-    <WorkoutContext.Provider value={{...state, dispatch}}>
+    <WorkoutContext.Provider value={{ ...state, dispatch }}>
       {children}
     </WorkoutContext.Provider>
   );
