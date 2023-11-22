ab1e3e4233acf32952983a9bc8a9067fdd52f06c
Kim Löfgren <lofgrenkim@outlook.com>
2023-11-22 21:58:33 +0200

    MERN userauth 2

--- week5/training/mern-stack/backend/server.js
+++ week5/training/mern-stack/backend/server.js
@@ -3,7 +3,7 @@ require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
 const workoutRoutes = require("./routes/wourkouts");
-
+const userRoutes = require("./routes/user");
 const port = process.env.PORT;
 
 // express app
@@ -19,6 +19,7 @@ app.use((req, res, next) => {
 
 // routes
 app.use("/api/workouts", workoutRoutes);
+app.use("/api/user/", userRoutes);
 
 // connect to db
 mongoose
