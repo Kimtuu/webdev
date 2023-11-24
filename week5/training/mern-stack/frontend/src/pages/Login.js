1dd064513b7e964e537bbe250ad75a35817c2802
Kim Löfgren <lofgrenkim@outlook.com>
2023-11-24 18:30:07 +0200

    MERN auth 12 loginHook

--- week5/training/mern-stack/frontend/src/pages/Login.js
+++ week5/training/mern-stack/frontend/src/pages/Login.js
@@ -1,16 +1,18 @@
 import { useState } from "react";
-
+import { useLogin } from "../hooks/useLogin";
 const Login = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const { login, error, isLoading } = useLogin();
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    console.log(email, password);
+
+    await login(email, password);
   };
 
   return (
-    <form className="login" onSubmit={ handleSubmit }>
+    <form className="login" onSubmit={handleSubmit}>
       <h3>Login</h3>
 
       <label>Email:</label>
@@ -26,7 +28,8 @@ const Login = () => {
         onChange={(e) => setPassword(e.target.value)}
         value={password}
       ></input>
-      <button>Login</button>
+      <button disabled={isLoading}>Login</button>
+      {error && <div className="error">{error} </div>}
     </form>
   );
 };
