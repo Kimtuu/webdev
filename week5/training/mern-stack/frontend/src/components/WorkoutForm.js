fd544b4f03b3154aedcef2da989a1628c21e7478
Kim Löfgren <lofgrenkim@outlook.com>
2023-11-24 19:33:56 +0200

    MERN auth 15 making auth reqs

--- week5/training/mern-stack/frontend/src/components/WorkoutForm.js
+++ week5/training/mern-stack/frontend/src/components/WorkoutForm.js
@@ -1,8 +1,10 @@
 import { useState } from "react";
 import { useWorkoutsContext } from "../hooks/useWorkoutsContext";
+import { useAuthContext } from "../hooks/useAuthContext";
 
 function WorkoutForm() {
   const { dispatch } = useWorkoutsContext();
+  const { user } = useAuthContext();
   const [title, setTitle] = useState("");
   const [load, setLoad] = useState("");
   const [reps, setReps] = useState("");
@@ -12,6 +14,11 @@ function WorkoutForm() {
   const handleSubmit = async (e) => {
     e.preventDefault();
 
+    if (!user) {
+      setError("You must be logged in");
+      return;
+    }
+
     const workout = { title, load, reps };
 
     const response = await fetch("/api/workouts", {
@@ -19,6 +26,7 @@ function WorkoutForm() {
       body: JSON.stringify(workout),
       headers: {
         "Content-Type": "application/json",
+        Authorization: `BEARER ${user.token}`,
       },
     });
     const json = await response.json();
--- week5/training/mern-stack/frontend/src/pages/Home.js
+++ week5/training/mern-stack/frontend/src/pages/Home.js
@@ -1,5 +1,6 @@
-import { useEffect} from "react";
+import { useEffect } from "react";
 import { useWorkoutsContext } from "../hooks/useWorkoutsContext";
+import { useAuthContext } from "../hooks/useAuthContext";
 
 // components
 
@@ -7,19 +8,26 @@ import WorkoutDetails from "../components/WorkoutDetails";
 import WorkoutForm from "../components/WorkoutForm";
 
 function Home() {
-  const {workouts, dispatch} = useWorkoutsContext()
+  const { workouts, dispatch } = useWorkoutsContext();
+  const { user } = useAuthContext();
 
   useEffect(() => {
-    const fethWorkouts = async () => {
-      const response = await fetch("/api/workouts");
+    const fetchWorkouts = async () => {
+      const response = await fetch("/api/workouts", {
+        headers: {
+          "Authorization": `BEARER ${user.token}`
+        }
+      });
       const json = await response.json();
 
       if (response.ok) {
-        dispatch({type:"SET_WORKOUTS", payload: json})
+        dispatch({ type: "SET_WORKOUTS", payload: json });
       }
     };
-    fethWorkouts();
-  }, [dispatch]);
+    if (user) {
+      fetchWorkouts();
+    }
+  }, [dispatch, user]);
 
   return (
     <div className="home">
